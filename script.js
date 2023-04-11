311ee21cefa62526cb6286c1e4bf8ff1969ec6ea
martinbroenner <martinbro90@gmail.com>
2023-04-11 18:33:55 +0200

    changed js menu

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
 const mobilmenu = document.querySelector(".mobilmenu");
 
-
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mobilmenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  mobilmenu.classList.toggle("active");
+});
 
+window.addEventListener('scroll', () => {
+  const isMobilmenuActive = mobilmenu.classList.contains('active');
+  if (window.pageYOffset > 0) {
+    if (!isMobilmenuActive) {
+      hamburger.classList.remove('active');
+    }
+    mobilmenu.classList.remove('active');
+  } else {
+    if (!isMobilmenuActive) {
+      hamburger.classList.remove('active');
+    }
+  }
+});
